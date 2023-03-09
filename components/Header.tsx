28584eb5320ce0151e5a8a5f4f00889ec8f0578f
Cody <bittusmaths@gmail.com>
2023-03-09 09:11:41 +0530

    New small navbar popped

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,25 +3,78 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-      <div className="flex justify-between items-center space-x-2 font-bold px-5 py-5 border-b-2">
-        <Link href="/" className="flex flex-row space-x-6 ">
-          <Image
-            src="https://cdn.sanity.io/images/wub429kh/production/0e2f51b71b329e866d9a4fb29c0581fb2c65c354-2054x1263.png"
-            alt="logo"
-            width={50}
-            height={100}
-            className="object-fit"
-          />
-          <h1 className="font-bold text-2xl">POWERKRAFT</h1>
-        </Link>
-        <div className="flex space-x-4">
-          <Link href="/" className="text-gray-500 text-xl hover:text-black">
-            About
-          </Link>
-          <Link href="/" className="text-gray-500 text-xl hover:text-black">
-            Contact
-          </Link>
-        </div>
-      </div>
+        <nav className="p-3 border-gray-100 rounded bg-white">
+          <div className="container flex flex-wrap items-center justify-between mx-auto">
+            <a href="#" className="flex items-center">
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="h-6 mr-3 sm:h-10"
+                alt="Flowbite Logo"
+              />
+              <span className="self-center text-xl font-semibold whitespace-nowrap">
+                POWERKRAFT
+              </span>
+            </a>
+            <button
+              data-collapse-toggle="navbar-hamburger"
+              type="button"
+              className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-hamburger"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <div className="hidden w-full" id="navbar-hamburger">
+              <ul className="flex flex-col mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:bg-blue-600"
+                    aria-current="page"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Services
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Pricing
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
     );
 }
