94b13548e20998e53d8c74a899dd821cd65b2df6
Cody <bittusmaths@gmail.com>
2023-03-09 13:25:17 +0530

    Certain changes related to Navbar which was last time added

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,13 +8,13 @@ export default function Header() {
   fluid={true}
   rounded={true}
 >
-  <Navbar.Brand href="https://flowbite.com/">
+  <Navbar.Brand href="/">
     <img
       src="https://flowbite.com/docs/images/logo.svg"
       className="mr-3 h-6 sm:h-9"
       alt="Flowbite Logo"
     />
-    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+    <span className="self-center whitespace-nowrap text-xl font-semibold">
       POWERKRAFT
     </span>
   </Navbar.Brand>
@@ -22,14 +22,15 @@ export default function Header() {
   <Navbar.Collapse>
     <Navbar.Link
       href="/navbars"
-      active={true}
+          active={true}
+           className="text-lg"
     >
       Home
     </Navbar.Link>
-    <Navbar.Link href="/navbars">
+    <Navbar.Link href="/navbars"  className="text-lg">
       About
     </Navbar.Link>
-    <Navbar.Link href="/navbars">
+    <Navbar.Link href="/navbars"  className="text-lg">
       Contact
     </Navbar.Link>
   </Navbar.Collapse>
