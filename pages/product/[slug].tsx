1cc2913dde5cd918724972794c493d4ece382e89
Cody <bittusmaths@gmail.com>
2023-03-10 08:34:38 +0530

    New structure for [slug].js

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -8,28 +8,47 @@ import urlFor from "@/lib/urlFor";
 import PortableText from "react-portable-text";
 import Link from "next/link";
 
-const Post = ({post}: {
-  post: { title: string; description: string; mainImage: string, amazonLink : string };
+const Post = ({
+  post,
+}: {
+  post: {
+    title: string;
+    description: string;
+    mainImage: string;
+    amazonLink: string;
+  };
 }) => {
   return (
     <>
       {post && (
-        <article>
-          {post.mainImage && (
-            <div>
-              <Image
+        <section className="text-gray-600 body-font overflow-hidden">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="lg:w-4/5 mx-auto flex flex-wrap">
+              <img
+                alt="ecommerce"
+                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
                 src={urlFor(post.mainImage).url()}
-                width={250}
-                height={200}
-                alt={post.mainImage.alt}
               />
+              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                <h2 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                  {post.title}
+                </h2>
+                <div className="flex mb-4"></div>
+                <p className="leading-relaxed">
+
+                  {/* Description */}
+
+                </p>
+
+                <div className="flex">
+                  <Link className="flex ml-auto border-2 py-2 px-6 border-black  text-black text-xl translate-1 transition hover:scale-110 duration:1000 delay-100 ease-in-out" href={post.amazonLink}>
+                    Amazon ↗
+                  </Link>
+                </div>
+              </div>
             </div>
-          )}
-          <h2>{post.title}</h2>
-          <Link className="text-black" href={post.amazonLink}>
-            Amazon
-          </Link>
-        </article>
+          </div>
+        </section>
       )}
     </>
   );
