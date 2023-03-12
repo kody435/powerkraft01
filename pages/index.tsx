21138b198980bdbabe6d3e1c193fb73f161701d4
Cody <bittusmaths@gmail.com>
2023-03-12 23:48:47 +0530

    Removed that dummy blue color

--- pages/index.tsx
+++ pages/index.tsx
@@ -114,7 +114,7 @@ const Index = ({ products, sliders }: TProducts) => {
         <div className=" bg-blue-400">{testSlider}</div>
         {/* // ! Now used  */}
         {/* <Slider/> */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6 mx-8 my-4 md:m-16 bg-blue-600">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-6 mx-8 my-4 md:m-16">
           {productList}
         </div>
       </div>
